ab568c1c860453b72457512651932779737f1bb5
sattosan <sattosan@users.noreply.github.com>
2022-12-04 08:48:36 +0000

    Apply Code Formatter Change

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
+import collections
+import time
+
 import config
 import spacy
 import utils
 from tqdm import tqdm
-import collections
-import time
-
-
 
 # 日本語辞書の読み込み
-nlp = spacy.load('ja_ginza')
+nlp = spacy.load("ja_ginza")
 # 品詞カウント用
 pos_counter = collections.Counter()
 # 品詞の出現回数をカウント
 def count_pos_of_token(keyword):
     try:
-        token_pos_pair = ''
+        token_pos_pair = ""
         # 形態素解析
-        nlp_keyword=nlp(keyword)
+        nlp_keyword = nlp(keyword)
         for sent in nlp_keyword.sents:
             for token in sent:
-                token_pos_pair+=f'{token.text}:{token.pos_}|'
-                pos_counter[token.pos_]+=1
-        return {'keyword': keyword, 'token_pos': token_pos_pair}
+                token_pos_pair += f"{token.text}:{token.pos_}|"
+                pos_counter[token.pos_] += 1
+        return {"keyword": keyword, "token_pos": token_pos_pair}
     except Exception:
-        return {'keyword': keyword, 'token_pos': 'None'}
+        return {"keyword": keyword, "token_pos": "None"}
